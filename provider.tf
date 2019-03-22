fff5d60e16ca4ae8e3fd4d92d6487022c3ff36ae
godstaker <alvin.vassoodaven@gmail.com>
2019-03-22 10:17:02 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -4,15 +4,33 @@ provider "google" {
   project     = "847662829465"
   region      = "europe-west1-d"
 }
-resource "google_compute_instance" "default" {
-  count        = "4"
-  name         = "test-${count.index}"
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
   machine_type = "f1-micro"
-  zone         = "europe-west1-d"
-  disk {
-    image       = "centos-7-v20170426"
-}
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
   network_interface {
-    network = "default"
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config = {
+    }
+  }
 }
-}
+#resource "google_compute_instance" "default" {
+#  count        = "4"
+#  name         = "test-${count.index}"
+#  machine_type = "f1-micro"
+#  zone         = "europe-west1-d"
+#  disk {
+#    image       = "centos-7-v20170426"
+#}
+#  network_interface {
+#    network = "default"
+#}
+#}
